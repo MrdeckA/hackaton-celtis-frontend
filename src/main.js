71401dd2f86ab7b906bea74f9a7f98af8b696715
Mériadeck AMOUSSOU <mrdeck30@gmail.com>
2024-12-09 13:26:34 +0100

    setup vuetify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,42 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@mdi/font/css/materialdesignicons.css";
 
-createApp(App).mount('#app')
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const light = {
+  dark: false,
+  colors: {
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
+    "surface-bright": "#FFFFFF",
+    "surface-light": "#EEEEEE",
+    "surface-variant": "#424242",
+    "on-surface-variant": "#EEEEEE",
+    primary: "#1f3b71",
+    "primary-darken-1": "#1F5592",
+    secondary: "#93c01f ",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "light",
+    themes: {
+      light,
+    },
+  },
+});
+
+createApp(App).use(vuetify).mount("#app");
