4d6613fdf71423521543e67a145f968527922a76
Mériadeck AMOUSSOU <mrdeck30@gmail.com>
2024-12-10 20:35:27 +0100

    o1f

--- src/router/index.js
+++ src/router/index.js
@@ -4,21 +4,65 @@ import HomeView from "../views/HomeView.vue";
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "HomeView",
+    component: () => import("../views/HomeView.vue"),
+  },
+  // {
+  //   path: "/OnboardingStepOne",
+  //   name: "OnboardingStepOne",
+  //   component: OnboardingStepOne,
+  // },
+  // {
+  //   path: "/OnboardingStepTwo",
+  //   name: "OnboardingStepTwo",
+  //   component: OnboardingStepTwo,
+  // },
+  {
+    path: "/user/home",
+    name: "UserHomeView",
+    component: () => import("../views/user/UserHome.vue"),
+  },
+  {
+    path: "/user/notifications",
+    name: "UserNotificationsView",
+    component: () => import("../views/user/NotificationView.vue"),
+  },
+  {
+    path: "/user/plans",
+    name: "UserPlanView",
+    component: () => import("../views/user/PlanView.vue"),
+  },
+  {
+    path: "/user/tontines",
+    name: "UserTontinesView",
+    component: () => import("../views/user/TontinesView.vue"),
+  },
+  {
+    path: "/user/account",
+    name: "UserAccountView",
+    component: () => import("../views/user/AccountView.vue"),
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ "../views/AboutView.vue");
-    },
+    component: () => import("../views/AboutView.vue"),
+  },
+  {
+    path: "/user/tontines/create",
+    name: "UserCreateTontine",
+    component: () => import("../views/user/tontines/TontineAdd.vue"),
+  },
+  {
+    path: "/details/:id",
+    name: "UserDetailsView",
+    component: () => import("../views/user/details.vue"),
+  },
+  {
+    path: "/user/tontines/:id",
+    name: "TontineDetailsView",
+    component: () => import("../views/user/tontines/TontineDetailView.vue"),
   },
 ];
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
