9cc8ba3666407b0eca33fea699a14e8f8d6dffdd
Mériadeck AMOUSSOU <mrdeck30@gmail.com>
2024-12-10 20:59:20 +0100

    ok 1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <router-view />
 </template>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,16 +11,25 @@
   color: #2c3e50;
 }
 
+html,
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  /* background-color: #f2f2f2; */
+  background-color: whitesmoke;
+}
+
 nav {
   padding: 30px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
